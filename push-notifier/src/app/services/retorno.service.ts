a3dba486565212caeb3a938dcd44bc767d89e050
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2023-12-31 20:12:21 -0300

    quase certo

--- push-notifier/src/app/services/retorno.service.ts
+++ push-notifier/src/app/services/retorno.service.ts
@@ -1,131 +1,16 @@
 import { Retorno } from './../model/retorno';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { ConexaoService } from './conexao.service';
-import Dexie from 'dexie';
+import { Injectable, Injector } from '@angular/core';
+import { TableService } from './table.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RetornoService {
-  
-  private API_RETORNO = 'http://localhost:8090/piante/retorno';  
-  private indexdDB: Dexie | undefined;
-  private table: Dexie.Table<Retorno, any> | undefined;
+export class RetornoService extends TableService<Retorno> { 
 
   constructor( 
-    private http: HttpClient, 
-    public conexao: ConexaoService
+    protected override injector: Injector
   ) {
-    this.ouvirStatusConexao();
-    this.iniciarIndexedDB();
+    super(injector, 'retorno', 'http://localhost:8090/piante/retorno');
   }
-
-  private iniciarIndexedDB() {
-    this.indexdDB = new Dexie('db-retorno');
-    this.indexdDB.version(1).stores({
-      retorno: 'recebimento'
-    });
-    this.table = this.indexdDB.table('retorno');
-  }
-
-
-
-
-  private salvarNaAPI(retorno: Retorno) {
-    this.http.post<Retorno>(this.API_RETORNO, retorno)
-      .subscribe(
-        () => console.log('retorno enviado para a API'),
-        (err) => console.log('erro ao salvar na API')
-      );
-  }
-
-
-  private async salvarNoIndexDB(retorno: Retorno) {
-    try {  
-      await this.table?.add(retorno);
-      //alert('dados salvos no indexedDB com sucesso !')
-    } catch {
-      //alert('erro ao salvar no indexedDB');      
-    }
-  }
-
-  private async enviarIndexedDBparaAPI() {   
-        
-    const todosRetornos = await this.table?.toArray();
-    if (todosRetornos) {
-      for (const retorno of todosRetornos) {
-        this.salvarNaAPI(retorno);
-        //this.table!.delete(retorno.recebimento);
-        //alert('banco local apagado');
-      }
-    }
-    this.table?.clear();
-    //alert('tabela apagada do indexedDB');
-  }
-
-  public salvar(retorno: Retorno) {
-    if (this.conexao.isOnline) {
-      this.salvarNaAPI(retorno);
-      //alert('retorno service chamou salvaNaApi');
-    } else {
-      this.salvarNoIndexDB(retorno);
-      //alert('retorno service chamou salvaNoIndexedDB');
-    }
-  }
-
-
-  private ouvirStatusConexao() {
-    this.conexao.statusConexao
-    .subscribe(online => {
-      if (online) {
-        this.enviarIndexedDBparaAPI();
-        //console.log('estou on-line, vou enviar do banco local pra api');        
-        //alert('retorno service chamou enviarIndexedDB para Api');
-      } else {        
-        //console.log('estou off-line, vou salvar no banco local');
-        //alert('retorno service vai salvar no indexedDB...');
-      }
-    })
-  }
-
-
-
-
-
-
-
-
-
-
-  
-  /* ************************************************************************
-
-
-  public cadastrar(obj: Retorno): Observable<Retorno> {
-    return this.http.post<Retorno>(this.API_RETORNO, obj);
-  }
-
-  public selecionar(): Observable<Retorno[]> {
-    return this.http.get<Retorno[]>(this.API_RETORNO);
-  }
-
-  public entidadeById(idretorno: number): Observable<Retorno> {
-    return this.http.get<Retorno>(this.API_RETORNO + '/' + idretorno).pipe();
-  }
-
-  public editar(obj: Retorno): Observable<Retorno> {
-    return this.http.put<Retorno>(this.API_RETORNO, obj);
-  }
-
-  public remover(idretorno: number): Observable<void> {
-    return this.http.delete<void>(this.API_RETORNO + '/' + idretorno);
-  }
-  
-  public buscar(idnotification: number): Observable<Retorno> {
-    return this.http.get<Retorno>(this.API_RETORNO);
-  } 
-
-  */
 
 }
---
+++ push-notifier/src/app/services/table.service.ts
@@ -0,0 +1,97 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, Injector } from '@angular/core';
+import Dexie from 'dexie';
+import { ConexaoService } from './conexao.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class TableService<T> {     // <T extends {id: string}>
+
+  private indexdDB: Dexie | undefined;
+  private table: Dexie.Table<T, any> | undefined;
+  
+  protected http: HttpClient; 
+  protected conexao: ConexaoService;
+
+  constructor(
+    protected injector: Injector,
+    protected nomeTabela: string,
+    protected urlApi: string
+  ) { 
+    this.http = this.injector.get(HttpClient);
+    this.conexao = this.injector.get(ConexaoService);
+
+    this.ouvirStatusConexao();
+    this.iniciarIndexedDB();
+  }
+
+  private iniciarIndexedDB() {
+    this.indexdDB = new Dexie('db-retorno');
+    this.indexdDB.version(1).stores({
+      [this.nomeTabela]: 'recebimento'
+    });
+    this.table = this.indexdDB.table(this.nomeTabela);
+  }
+
+
+
+
+  private salvarNaAPI(table: T) {
+    this.http.post<T>(this.urlApi, table)
+      .subscribe(
+        () => console.log('table enviado para a API', table),
+        (err) => console.log('erro ao salvar na API', table)
+      );
+  }
+
+
+  private async salvarNoIndexDB(table: T) {
+    try {  
+      await this.table?.add(table);      
+      const todaTabela = await this.table?.toArray();
+      console.log('dados salvos no indexedDB com sucesso !', todaTabela);
+    } catch (error) {
+      console.log('erro ao salvar no indexedDB', error);     
+    }
+  }
+
+  private async enviarIndexedDBparaAPI() {           
+    const todaTabela = await this.table?.toArray();
+    if (todaTabela) {
+      for (const table of todaTabela) {
+        this.salvarNaAPI(table);
+        //this.table!.delete(table.id);
+        //alert('banco local apagado');
+      }
+    }
+    await this.table?.clear();
+    //alert('tabela apagada do indexedDB');
+  }
+
+  public salvar(table: T) {
+    if (this.conexao.isOnline) {
+      this.salvarNaAPI(table);
+      //alert('table service chamou salvaNaApi');
+    } else {
+      this.salvarNoIndexDB(table);
+      //alert('table service chamou salvaNoIndexedDB');
+    }
+  }
+
+
+  private ouvirStatusConexao() {
+    this.conexao.statusConexao
+    .subscribe(online => {
+      if (online) {
+        this.enviarIndexedDBparaAPI();
+        console.log('estou on-line, vou enviar do banco local pra api');        
+        //alert('table service chamou enviarIndexedDB para Api');
+      } else {        
+        console.log('estou off-line, vou salvar no banco local');
+        //alert('table service vai salvar no indexedDB...');
+      }
+    })
+  }
+
+}
