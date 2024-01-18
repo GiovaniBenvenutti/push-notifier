d85ad79d45aec147e5309c52e88f19f6ce86043e
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2024-01-18 12:40:12 -0300

    resquest subscription

--- push-notifier/src/app/app.component.ts
+++ push-notifier/src/app/app.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SwUpdate } from '@angular/service-worker';
+import { SwPush, SwUpdate } from '@angular/service-worker';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,34 @@ export class AppComponent implements OnInit{
 
   title = 'push-notifier';
 
-  constructor(private router: Router, private swUpDate: SwUpdate){}
+  constructor(
+      private router: Router,
+      private swUpDate: SwUpdate,
+
+
+      private http: HttpClient,
+      private swPush: SwPush
+  ){
+
+    //      AQUI COMECEI A IMPLANTAR O RECEBIMENTO DAS PUSH NOTIFICATION...
+
+    this.swPush.requestSubscription({
+      serverPublicKey: ''
+    })
+    .then(subscription => {
+      console.log('usuario permitu envio de notificações');
+      this.http.post('http://localhost:9000/add-subscription', subscription)
+        .subscribe();
+    })
+    .catch(err => {
+      console.error('usuario recusou o recebimento denotificações', err);
+    });
+
+
+
+
+    //      AQUI TERMINOU A TENTATIVA
+  }
 
   reloadCache(){
     if(this.swUpDate.isEnabled){
@@ -30,6 +59,16 @@ export class AppComponent implements OnInit{
   navigateTo(route: string) {
     this.router.navigate([route]);    
   }    
+
+
+
+
+
+
+
+
+
+
 
 
 
